44b4344e7976ece13d8e6625861713b314f98f2b
sgruenheit <styrmir.gruenheit@bridging-it.de>
2025-04-17 07:50:32 +0200

    Added chat widget (wip)

--- paia-frontend/src/app/widget/chat/chat.component.ts
+++ paia-frontend/src/app/widget/chat/chat.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import {Component, CUSTOM_ELEMENTS_SCHEMA} from '@angular/core';
+import 'deep-chat';
 
 @Component({
   selector: 'app-chat',
   imports: [],
   templateUrl: './chat.component.html',
-  styleUrl: './chat.component.scss'
+  styleUrl: './chat.component.scss',
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class ChatComponent {
 
