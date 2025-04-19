299b295fb1a1f206f853f1d4381a984e39632b3c
sgruenheit <styrmir.gruenheit@bridging-it.de>
2025-04-19 11:45:17 +0200

    Feat: Upgrade to LangChain4j beta-3

--- paia-backend/src/main/java/com/paia/backend/features/chatbot/config/ChatBotConfig.java
+++ paia-backend/src/main/java/com/paia/backend/features/chatbot/config/ChatBotConfig.java
@@ -1,6 +1,8 @@
 package com.paia.backend.features.chatbot.config;
 
 import com.paia.backend.features.chatbot.service.ChatBotAIService;
+import dev.langchain4j.memory.ChatMemory;
+import dev.langchain4j.memory.chat.MessageWindowChatMemory;
 import dev.langchain4j.model.chat.StreamingChatLanguageModel;
 import dev.langchain4j.service.AiServices;
 import dev.langchain4j.service.tool.ToolProvider;
@@ -11,12 +13,18 @@ import org.springframework.context.annotation.Configuration;
 public class ChatBotConfig {
 
     @Bean
-    ChatBotAIService chatBotAIService(StreamingChatLanguageModel streamingChatLanguageModel, ToolProvider toolProvider) {
+    ChatBotAIService chatBotAIService(StreamingChatLanguageModel streamingChatLanguageModel, ToolProvider toolProvider, ChatMemory chatMemory) {
         return AiServices
                 .builder(ChatBotAIService.class)
                 .streamingChatLanguageModel(streamingChatLanguageModel)
                 .toolProvider(toolProvider)
+                .chatMemory(chatMemory)
                 .build();
+    }
+
+    @Bean
+    public ChatMemory chatMemory() {
+        return MessageWindowChatMemory.builder().maxMessages(30).build();
     }
 
 }
